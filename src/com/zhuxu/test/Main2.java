8923ce8157c2dc81d5cf9289a719d06e6538c881
mac <mac@192.168.1.100>
2016-05-11 10:52:10 +0800

    略微修改

--- src/com/zhuxu/test/Main2.java
+++ src/com/zhuxu/test/Main2.java
@@ -1,7 +1,13 @@
 package com.zhuxu.test;
 
+import java.util.Arrays;
+import java.util.Date;
+import  java.lang.System;
+
 public class Main2 {
 	public static void main(String[] args) {
-		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
 	}
 }
